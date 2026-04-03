3624635072eff5db3ee4747285a7462dc694397e
Denmark Escobanez <denmark25escobanez@gmail.com>
2026-04-03 10:48:55 +0800

    fix vite dist path

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ const { connectToMongoDB } = require("./database");
 const path = require("path");
 
 const app = express();
+
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 const router = require("./routes");
 app.use("/api", router);
+
+/* serve frontend */
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +23,7 @@ const startServer = async () => {
   await connectToMongoDB();
 
   app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
   });
 };
 
