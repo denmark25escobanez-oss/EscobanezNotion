90eeb215edeebbdd33b71d25dfe137b1884e851e
Denmark Escobanez <denmark25escobanez@gmail.com>
2026-04-03 18:16:30 +0800

    fix: update static paths for vite deployment

--- server/index.js
+++ server/index.js
@@ -6,13 +6,16 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
-});
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 const router = require("./routes");
 app.use("/api", router);
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
@@ -22,4 +25,5 @@ const startServer = async () => {
         console.log(`Server is listening on http://localhost:${port}`);
     });
 };
+
 startServer();
